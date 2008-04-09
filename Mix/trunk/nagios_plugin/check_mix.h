7ee5e5e7afa48b85b0cd43655920603953adc850
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-04-09 13:25:58 +0000

    started nagios plugin for Mix Monitoring

---
+++ Mix/trunk/nagios_plugin/check_mix.h
@@ -0,0 +1,10 @@
+#ifndef CHECK_MIX_H_
+#define CHECK_MIX_H_
+
+#define CMP_EQUAL 0
+#define STATUS_TYPE_FOUND 0
+#define NO_STATUS_TYPE_FOUND 1
+
+static int setCurrentStatusType(status_type_t *currStatusTypePtr, const char *name);
+
+#endif /*CHECK_MIX_H_*/
