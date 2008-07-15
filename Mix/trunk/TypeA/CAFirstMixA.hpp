b00c57f300388817b18753a18d24ce1d3012f6cb
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-07-15 09:51:19 +0000

    reverted first mix cleanup

--- Mix/trunk/TypeA/CAFirstMixA.hpp
+++ Mix/trunk/TypeA/CAFirstMixA.hpp
@@ -36,17 +36,6 @@ class CAFirstMixA:public CAFirstMix
 		public:
 	
 		virtual void shutDown();
-		virtual ~CAFirstMixA()
-		{
-			//clean();
-			delete m_pmutexUser;
-			m_pmutexUser = NULL;
-			delete m_pmutexMixedPackets;
-			m_pmutexMixedPackets = NULL;
-			delete m_pmutexLoginThreads;
-			m_pmutexLoginThreads = NULL;
-		}
-		virtual SINT32 clean();
 
 		protected:
 			SINT32 loop();	
