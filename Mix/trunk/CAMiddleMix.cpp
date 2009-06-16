9e22022ba4999aaccb992ce09e1c5338aa870765
simon <simon@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2009-06-16 09:55:59 +0000

    extended the size of key data exchanged between mixes from 2 to 4 bytes (works but a bit more testing is still recommended)

--- Mix/trunk/CAMiddleMix.cpp
+++ Mix/trunk/CAMiddleMix.cpp
@@ -81,17 +81,17 @@ SINT32 CAMiddleMix::initOnce()
 SINT32 CAMiddleMix::processKeyExchange()
 	{
 		UINT8* recvBuff=NULL;
-		UINT16 len;
+		UINT32 len;
 		SINT32 ret;
 
-		if(m_pMuxOut->getCASocket()->receiveFully((UINT8*)&len,2)!=E_SUCCESS)
+		if(m_pMuxOut->getCASocket()->receiveFully((UINT8*)&len, sizeof(len))!=E_SUCCESS)
 			{
-				CAMsg::printMsg(LOG_INFO,"Error receiving Key Info lenght from Mix n+1!\n");
+				CAMsg::printMsg(LOG_INFO,"Error receiving Key Info length from Mix n+1!\n");
 				MONITORING_FIRE_NET_EVENT(ev_net_keyExchangeNextFailed);
 				return E_UNKNOWN;
 			}
-		len=ntohs(len);
-		CAMsg::printMsg(LOG_INFO,"Received Key Info lenght %u\n",len);
+		len=ntohl(len);
+		CAMsg::printMsg(LOG_INFO,"Received Key Info length %u\n",len);
 		recvBuff=new UINT8[len+1]; //for the \0 at the end
 		if(recvBuff==NULL)
 			return E_UNKNOWN;
@@ -209,9 +209,10 @@ SINT32 CAMiddleMix::processKeyExchange()
 						m_pMuxOut->setReceiveKey(key+32,32);
 						UINT32 outlen=0;
 						UINT8* out=DOM_Output::dumpToMem(docSymKey,&outlen);
-						UINT16 size=htons((UINT16)outlen);
-						m_pMuxOut->getCASocket()->send((UINT8*)&size,2);
-						m_pMuxOut->getCASocket()->send(out,outlen);
+						CAMsg::printMsg(LOG_DEBUG,"send length %u\n", outlen);
+						UINT32 size=htonl(outlen);
+						m_pMuxOut->getCASocket()->send((UINT8*)&size, sizeof(size));
+						m_pMuxOut->getCASocket()->send(out, outlen);
 						if (docSymKey != NULL)
 						{
 							docSymKey->release();
@@ -303,36 +304,39 @@ SINT32 CAMiddleMix::processKeyExchange()
 
 
 
-		UINT8* out=new UINT8[0xFFFF];
-		memset(out, 0, (sizeof(UINT8)*0xFFFF));
-		UINT32 outlen=0xFFFD;
-		DOM_Output::dumpToMem(doc,out+2,&outlen);
+		//UINT8* out=new UINT8[0xFFFF];
+		//memset(out, 0, (sizeof(UINT8)*0xFFFF));
+		UINT32 outlen = 0;
+		UINT8* out = DOM_Output::dumpToMem(doc, &outlen);
 #ifdef _DEBUG
-		CAMsg::printMsg(LOG_DEBUG,"New Key Info size: %u\n",outlen);
+		CAMsg::printMsg(LOG_DEBUG,"New Key Info size: %u\n", outlen);
 #endif
-		len=htons((UINT16)outlen);
-		memcpy(out,&len,2);
-		ret=m_pMuxIn->getCASocket()->send(out,outlen+2);
+		len = htonl(outlen);
+		//memcpy(out,&len, sizeof(len));
+
+		m_pMuxIn->getCASocket()->send((UINT8*) &len, sizeof(len));
+
+		ret=m_pMuxIn->getCASocket()->send(out, outlen);
 		delete[] out;
 		out = NULL;
-		if(ret<0||(UINT32)ret!=outlen+2)
+		if( (ret < 0) || (ret != outlen) )
 		{
 			CAMsg::printMsg(LOG_DEBUG,"Error sending new New Key Info\n");
 			MONITORING_FIRE_NET_EVENT(ev_net_keyExchangeNextFailed);
 			return E_UNKNOWN;
 		}
 		MONITORING_FIRE_NET_EVENT(ev_net_keyExchangeNextSuccessful);
-		CAMsg::printMsg(LOG_DEBUG,"Sending new New Key Info succeded\n");
+		CAMsg::printMsg(LOG_DEBUG,"Sending new key info succeeded\n");
 
 		//Now receiving the symmetric key form Mix n-1
-		m_pMuxIn->getCASocket()->receive((UINT8*)&len,2);
-		len=ntohs(len);
-		recvBuff=new UINT8[len+1]; //for \0 at the end
-		if(m_pMuxIn->getCASocket()->receive(recvBuff,len)!=len)
+		m_pMuxIn->getCASocket()->receive((UINT8*) &len, sizeof(len));
+		len = ntohl(len);
+		recvBuff = new UINT8[len+1]; //for \0 at the end
+		if(m_pMuxIn->getCASocket()->receive(recvBuff, len) != len)
 		{
 
 			MONITORING_FIRE_NET_EVENT(ev_net_keyExchangePrevFailed);
-			CAMsg::printMsg(LOG_ERR,"Error receiving symetric key from Mix n-1!\n");
+			CAMsg::printMsg(LOG_ERR,"Error receiving symmetric key from Mix n-1!\n");
 			delete []recvBuff;
 			recvBuff = NULL;
 			return E_UNKNOWN;
@@ -341,7 +345,7 @@ SINT32 CAMiddleMix::processKeyExchange()
 		CAMsg::printMsg(LOG_INFO,"Symmetric Key Info received is:\n");
 		CAMsg::printMsg(LOG_INFO,"%s\n",(char*)recvBuff);
 		//Parsing doc received
-		doc=parseDOMDocument(recvBuff,len);
+		doc=parseDOMDocument(recvBuff, len);
 		delete[] recvBuff;
 		recvBuff = NULL;
 		if(doc==NULL)
@@ -360,7 +364,7 @@ SINT32 CAMiddleMix::processKeyExchange()
 		if(oSig.verifyXML(elemRoot)!=E_SUCCESS)
 		{
 			MONITORING_FIRE_NET_EVENT(ev_net_keyExchangePrevFailed);
-			CAMsg::printMsg(LOG_CRIT,"Could not verify the symetric key form Mix n-1!\n");
+			CAMsg::printMsg(LOG_CRIT,"Could not verify the symmetric key from Mix n-1!\n");
 			return E_UNKNOWN;
 		}
 		//Verifying nonce
