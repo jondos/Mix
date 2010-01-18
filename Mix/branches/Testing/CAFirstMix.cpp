51c2a83951b809c2d4eed4a4d776f31352785736
rolf <rolf@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2010-01-18 23:09:20 +0000

    small debug message change

--- Mix/branches/Testing/CAFirstMix.cpp
+++ Mix/branches/Testing/CAFirstMix.cpp
@@ -303,8 +303,8 @@ SINT32 CAFirstMix::connectToNextMix(CASocketAddr* a_pAddrNext)
 				
 			if (errLast != err || i % 10 == 0)
 			{
-				CAMsg::printMsg(LOG_ERR, "Cannot connect to next Mix. Reason: %s (%i). Retrying... (You will be notified on success or if a different error occurs.)\n",
-					GET_NET_ERROR_STR(err), err);
+				CAMsg::printMsg(LOG_ERR, "Cannot connect to next Mix on %s. Reason: %s (%i). Retrying... (You will be notified on success or if a different error occurs.)\n",
+					buff, GET_NET_ERROR_STR(err), err);
 				errLast = err;
 			}
 			else
